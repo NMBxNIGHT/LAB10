733eebf015610fb0d7604b0f03a4284aa8dd2adc
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2022-12-27 15:34:38 +0700

    ready 2 use leaw kub

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){	
+	int 	EndOfYear=1;
+	double PrevBalance,Interest,Total,NewBalance,rate,PPY; //PPY=pay per year
 	cout << "Enter initial loan: ";
+	cin >> PrevBalance;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> PPY;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,31 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+	NewBalance = PrevBalance;
+	while(NewBalance>0){
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << EndOfYear;
+
+		cout << setw(13) << left << PrevBalance;
+
+		Interest = PrevBalance*rate/100.0;
+	
+		cout << setw(13) << left << Interest;
+		Total = PrevBalance+Interest;
+		cout << setw(13) << left << Total;
+		if(Total>PPY){
+			cout << setw(13) << left << PPY;
+		}else{
+			PPY = Total;
+			cout << setw(13) << left << PPY; 
+		}
+		NewBalance = Total - PPY;
+		cout << setw(13) << left << NewBalance << endl;
+
+		EndOfYear++;
+		PrevBalance= NewBalance;
+	}
 	
 	return 0;
 }
